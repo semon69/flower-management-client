8737b462b1b0de47b2c2134b31496117576ace04
unknown <smdemon3@gmail.com>
2024-03-05 22:12:35 +0600

    styled login and registration page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,6 +6,8 @@ import { useAppDispatch } from "../redux/hook";
 import { verifyToken } from "../utils/verfyToken";
 import { NavLink, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import Lottie from "lottie-react";
+import loginDataLottie from "../../public/registration.json";
 
 const Login = () => {
   const dispatch = useAppDispatch();
@@ -15,7 +17,6 @@ const Login = () => {
   const { register, handleSubmit } = useForm();
 
   const onSubmit = async (data: any) => {
-
     try {
       const userInfo = {
         email: data.email,
@@ -26,10 +27,10 @@ const Login = () => {
       const user = verifyToken(res.data.token) as TUser;
 
       dispatch(setUser({ user, token: res.data.token }));
-      toast.success('Login Success')
+      toast.success("Login Success");
       navigate("/");
     } catch (error) {
-        toast.error("Something went wrong", { duration: 2000 });
+      toast.error("Something went wrong", { duration: 2000 });
     }
   };
 
@@ -38,51 +39,56 @@ const Login = () => {
       style={{
         width: "100%",
         height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
       }}
+      className="md:flex md:justify-evenly items-center bg-slate-200"
     >
-      <div className="border-2 rounded-md p-5">
-      <p className="text-lg font-bold text-center text-green-600">Login Now</p>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div style={{ marginBottom: "1rem" }}>
-            <label className="font-bold" htmlFor="">
-              Email:
-            </label>
-            <input
-              className="input input-bordered input-info w-full max-w-xs"
-              placeholder="Enter yout Email"
-              type="text"
-              id="email"
-              {...register("email")}
-            />
-          </div>
-          <div style={{ marginBottom: "1rem" }}>
-            <label className="font-bold" htmlFor="">
-              Password:
-            </label>
-            <input
-              className="input input-bordered input-info w-full max-w-xs"
-              type="password"
-              placeholder="Enter your password"
-              {...register("password")}
-              id="password"
-            />
-          </div>
-          <button
-            className="cursor-pointer rounded bg-blue-600 py-2 px-4 text-white"
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
-        <p className="text-sm mt-4">
-          Don't have any account?
-          <span className="ml-2 text-blue-600">
-            <NavLink to={"/register"}>Register</NavLink>
-          </span>
-        </p>
+      <div className=" md:w-1/2 lg:text-left">
+        <Lottie animationData={loginDataLottie} />
+      </div>
+      <div>
+        <div className="border-2 rounded-md p-5 shadow-xl m-2 bg-white">
+          <p className="text-lg font-bold text-center text-green-600">
+            Login Now
+          </p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div style={{ marginBottom: "1rem" }}>
+              <label className="font-bold" htmlFor="">
+                Email:
+              </label>
+              <input
+                className="input input-bordered input-info w-full max-w-xs"
+                placeholder="Enter yout Email"
+                type="text"
+                id="email"
+                {...register("email")}
+              />
+            </div>
+            <div style={{ marginBottom: "1rem" }}>
+              <label className="font-bold" htmlFor="">
+                Password:
+              </label>
+              <input
+                className="input input-bordered input-info w-full max-w-xs"
+                type="password"
+                placeholder="Enter your password"
+                {...register("password")}
+                id="password"
+              />
+            </div>
+            <button
+              className="cursor-pointer rounded bg-blue-600 py-2 px-4 text-white"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+          <p className="text-sm mt-4">
+            Don't have any account?
+            <span className="ml-2 text-blue-600">
+              <NavLink to={"/register"}>Register</NavLink>
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,106 +4,105 @@ import { Controller, useForm } from "react-hook-form";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useRegisteredMutation } from "../redux/features/auth/authApi";
 import { toast } from "sonner";
+import Lottie from "lottie-react";
+import registrationLottie from "../../public/reg.json";
 
 const Register = () => {
   const { register, handleSubmit, control } = useForm();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [registered] = useRegisteredMutation();
 
   const onSubmit = async (userInfo: any) => {
     registered(userInfo);
-    toast.success('Registered successfully, Please Login Now')
-    navigate('/login')
+    toast.success("Registered successfully, Please Login Now");
+    navigate("/login");
   };
 
   return (
     <div
+      className="md:flex md:justify-evenly items-center bg-slate-200"
       style={{
         width: "100%",
         height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <div className="border-2 rounded-md p-5">
-        <p className="text-lg font-bold text-center text-green-600">Register Now</p>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
-          <div>
-            <label
-              className="font-bold"
+      <div className=" md:w-1/2 lg:text-left">
+        <Lottie animationData={registrationLottie} />
+      </div>
+      <div>
+        <div className="border-2 rounded-md p-5  shadow-xl m-2 bg-white">
+          <p className="text-lg font-bold text-center text-green-600">
+            Register Now
+          </p>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+            <div>
+              <label className="font-bold">Name:</label>
+              <input
+                className="input input-bordered input-info w-full max-w-xs"
+                placeholder="Enter yout Name"
+                type="text"
+                id="email"
+                {...register("name")}
+              />
+            </div>
+            <div>
+              <label className="font-bold">Gender:</label>
+              <Controller
+                name="gender"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <select
+                    className="input input-bordered input-info w-full max-w-xs"
+                    {...field}
+                  >
+                    <option value="">Select Gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="others">Others</option>
+                  </select>
+                )}
+              />
+            </div>
+            <div>
+              <label className="font-bold" htmlFor="">
+                Email:
+              </label>
+              <input
+                className="input input-bordered input-info w-full max-w-xs"
+                placeholder="Enter yout Email"
+                type="text"
+                id="email"
+                {...register("email")}
+              />
+            </div>
+            <div>
+              <label className="font-bold" htmlFor="">
+                Password:{" "}
+              </label>
+              <input
+                className="input input-bordered input-info w-full max-w-xs"
+                placeholder="Enter yout Password"
+                type="password"
+                {...register("password")}
+                id="password"
+              />
+            </div>
+            <button
+              className="cursor-pointer rounded bg-blue-600 py-2 px-4 text-white"
+              type="submit"
             >
-              Name:
-            </label>
-            <input
-              className="input input-bordered input-info w-full max-w-xs"
-              placeholder="Enter yout Name"
-              type="text"
-              id="email"
-              {...register("name")}
-            />
-          </div>
-          <div>
-            <label
-              className="font-bold"
-            >
-              Gender:
-            </label>
-            <Controller
-              name="gender"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <select
-                  className="input input-bordered input-info w-full max-w-xs"
-                  {...field}
-                >
-                  <option value="">Select Gender</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                  <option value="others">Others</option>
-                </select>
-              )}
-            />
-          </div>
-          <div>
-            <label className="font-bold" htmlFor="">
-              Email:
-            </label>
-            <input
-              className="input input-bordered input-info w-full max-w-xs"
-              placeholder="Enter yout Email"
-              type="text"
-              id="email"
-              {...register("email")}
-            />
-          </div>
-          <div>
-            <label className="font-bold" htmlFor="">
-              Password:{" "}
-            </label>
-            <input
-              className="input input-bordered input-info w-full max-w-xs"
-              placeholder="Enter yout Password"
-              type="password"
-              {...register("password")}
-              id="password"
-            />
-          </div>
-          <button
-            className="cursor-pointer rounded bg-blue-600 py-2 px-4 text-white"
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
-        <p className="text-sm mt-4">
-          Already have an account?
-          <span className="ml-2 text-blue-600">
-            <NavLink to={"/login"}>Login</NavLink>
-          </span>
-        </p>
+              Submit
+            </button>
+          </form>
+          <p className="text-sm mt-4">
+            Already have an account?
+            <span className="ml-2 text-blue-600">
+              <NavLink to={"/login"}>Login</NavLink>
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
